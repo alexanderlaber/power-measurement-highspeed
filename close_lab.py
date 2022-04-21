57627551ea23fb62b3d66e2c80dfe734e6c3b2ec
alaber <laber@uni-kassel.de>
2022-04-21 13:19:41 +0200

    dsf

--- close_lab.py
+++ close_lab.py
@@ -1,11 +1,12 @@
 import paho.mqtt.client as mqtt
 
-def close_lab():
+def close_lab(system_code, unit_code):
   client = mqtt.Client(client_id="hello")
   client.username_pw_set(username="digi-hw",password="tegut")
   client.connect("frackles")
-  client.publish("digi/433","11111 10000 off")
+  #client.publish("digi/433","11111 10000 off")
+  client.publish("digi/433",f"{system_code} {unit_code} off")
 
 
 if __name__=='__main__':
-  close_lab()
+  close_lab("11111", "10101")
--- start_lab.py
+++ start_lab.py
@@ -1,11 +1,12 @@
 import paho.mqtt.client as mqtt
 
-def start_lab():
+def start_lab(system_code, unit_code):
   client = mqtt.Client(client_id="hello")
   client.username_pw_set(username="digi-hw",password="tegut")
   client.connect("frackles")
-  client.publish("digi/433","11111 10000 on")
+  #client.publish("digi/433","11111 10000 on")
+  client.publish("digi/433",f"{system_code} {unit_code} on")
 
 
 if __name__=='__main__':
-  start_lab()
+  start_lab("11111", "10101")
